49c939f099b38599ff7515eae2bd2810150c8f9a
mykola-kolomoyets <kolomoyets473@gmail.com>
2023-05-28 18:31:35 +0300

    minor fix

--- src/hooks/useDocumentLockScrollY.ts
+++ src/hooks/useDocumentLockScrollY.ts
@@ -1,10 +1,12 @@
 export const useDocumentLockScrollY = () => {
   const lockScrollY = () => {
     document.body.style.overflowY = "hidden";
+    document.documentElement.style.overflowY = "hidden";
   };
 
   const unlockScrollY = () => {
     document.body.style.overflowY = "auto";
+    document.documentElement.style.overflowY = "auto";
   };
 
   return {
